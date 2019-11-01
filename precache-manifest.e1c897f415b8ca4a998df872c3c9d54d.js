2569d6c9ad1350b3bef60436f609999da276fb41
NikolayKanchev <nikolaj.kanchev@gmail.com>
2019-11-01 14:31:12 +0100

    gh-pages

---
+++ precache-manifest.e1c897f415b8ca4a998df872c3c9d54d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7d679953ff8544763f37092f15fb18",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "acff3106787b01ce3d47",
+    "url": "/weather-app/static/css/main.cd26db82.chunk.css"
+  },
+  {
+    "revision": "e6260b37d41932537973",
+    "url": "/weather-app/static/js/2.dc76caf5.chunk.js"
+  },
+  {
+    "revision": "acff3106787b01ce3d47",
+    "url": "/weather-app/static/js/main.6c7c9610.chunk.js"
+  },
+  {
+    "revision": "0fb95fa3ae9adf1e8e3c",
+    "url": "/weather-app/static/js/runtime-main.3834d3c7.js"
+  }
+]);
